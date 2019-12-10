243eb370b68a443e38344ad44ad0d9648bec3f88
anxoret <post.lux@yandex.ru>
2019-12-10 20:05:59 +0400

    New configurations of Webpack, but without configs for IE

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 "use strict";
 
+const path = require("path");
 const NODE_ENV = process.env.NODE_ENV || "development";
 const webpack = require("webpack");
 
 module.exports = {
     // mode: "development",
-    entry: "./js/es6/main.js",
+    entry: {
+        main: "./js/es6/main.js",
+    },
     output: {
-    //   path: path.resolve(__dirname, 'dist'),
-      filename: "build-es6.js" 
+        filename: "[name].js", 
+        path: path.resolve(__dirname, "/dist"),
+        publicPath: "/dist"
+    },
+
+    devServer: {
+        overlay: true
     },
 
     watch: NODE_ENV == "development",
@@ -17,11 +25,25 @@ module.exports = {
         aggregateTimeout: 100
     },
 
-    devtool: NODE_ENV == "development" ? "cheap-inline-module-source-map" : null,
+    devtool: NODE_ENV == "development" ? "source-map" : null,
 
     plugins: [
         new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
+            NODE_ENV: JSON.stringify(NODE_ENV),
+            LANG: JSON.stringify("ru")
         })
-    ]
-}
+    ],
+
+    module: {
+        rules: [{
+            test: /\.js$/, 
+            exclude: "/node_modules",
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        }]
+    }
+};
